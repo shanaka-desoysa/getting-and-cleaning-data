b5287a7b72c25f449d3fa8de1929abe54233b0fa
shanaka-desoysa <shanaka@computer.org>
2016-08-15 07:25:58 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,7 @@
-dataFolder = "./data"
-dataFile = "./data/Dataset.zip"
-dataFileUrl = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+dataFolder <- "./data"
+dataFile <- "./data/Dataset.zip"
+dataFileUrl <-
+    "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
 # Create data folder
 if (!file.exists(dataFolder)) {
@@ -21,39 +22,39 @@ if (file.exists(paste(dataFolder, "/UCI HAR Dataset", sep = ""))) {
 }
 
 # Features
-features = read.table(paste(dataFolder, "/features.txt", sep = ""),
-                      colClasses = c("integer", "character"))
-features = features[, 2]
+features <- read.table(paste(dataFolder, "/features.txt", sep = ""),
+                       colClasses = c("integer", "character"))
+features <- features[, 2]
 
 # Read Test dataset with labels
-test_x = read.table(
+test_x <- read.table(
     paste(dataFolder, "/test/X_test.txt", sep = ""),
     col.names = features,
     header = F
 )
-test_y = read.table(
+test_y <- read.table(
     paste(dataFolder, "/test/y_test.txt", sep = ""),
     col.names = "Activity",
     header = F
 )
-test_subject = read.table(
+test_subject <- read.table(
     paste(dataFolder, "/test/subject_test.txt", sep = ""),
     col.names = "Subject",
     header = F
 )
 
 # Read Training dataset with labels
-train_x = read.table(
+train_x <- read.table(
     paste(dataFolder, "/train/X_train.txt", sep = ""),
     col.names = features,
     header = F
 )
-train_y = read.table(
+train_y <- read.table(
     paste(dataFolder, "/train/y_train.txt", sep = ""),
     col.names = "Activity",
     header = F
 )
-train_subject = read.table(
+train_subject <- read.table(
     paste(dataFolder, "/train/subject_train.txt", sep = ""),
     col.names = "Subject",
     header = F
@@ -65,14 +66,14 @@ train_subject = read.table(
 #
 
 # Create Test data frame binding columns
-test = cbind(test_subject, test_y, test_x)
+test <- cbind(test_subject, test_y, test_x)
 
 # Create Training data frame binding columns
-train = cbind(train_subject, train_y, train_x)
+train <- cbind(train_subject, train_y, train_x)
 
 
 # Merge training and test datasets
-mergedData = rbind(train, test)
+mergedData <- rbind(train, test)
 
 # Cleanup intermediate variables
 rm(list = ls(pattern = "test"))
@@ -83,7 +84,8 @@ rm(features)
 #
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 #
-mergedData = mergedData[, grepl("\\.mean|\\.std|Activity|Subject", names(mergedData))]
+mergedData <-
+    mergedData[, grepl("\\.mean|\\.std|Activity|Subject", names(mergedData))]
 
 
 #
@@ -91,40 +93,40 @@ mergedData = mergedData[, grepl("\\.mean|\\.std|Activity|Subject", names(mergedD
 #
 
 # Activity labels
-activities = read.table(
+activities <- read.table(
     paste(dataFolder, "/activity_labels.txt", sep = ""),
     colClasses = c("integer", "character"),
     col.names = c("id", "Activity")
 )
-mergedData$Activity = factor(mergedData$Activity,
-                             levels = activities$id,
-                             labels = activities$Activity)
+mergedData$Activity <- factor(mergedData$Activity,
+                              levels = activities$id,
+                              labels = activities$Activity)
 
 
 #
 # 4. Appropriately labels the data set with descriptive variable names.
 #
 
-columns = colnames(mergedData)
-columns = sub("^t", "Time.", columns)
-columns = sub("^f", "Freq.", columns)
-columns = sub("(Body)+", "Body", columns)
-columns = sub("Acc", ".Accelaration", columns)
-columns = sub("Mag", ".Magnitude", columns)
-columns = sub("Gyro", ".AngularVelocity", columns)
-columns = sub("mean", "Mean", columns)
-columns = sub("std", "StandardDeviation", columns)
-columns = sub("\\.(\\.)+", "", columns)
-columns = sub("X$", ".X", columns)
-columns = sub("Y$", ".Y", columns)
-columns = sub("Z$", ".Z", columns)
+columns <- colnames(mergedData)
+columns <- sub("^t", "Time.", columns)
+columns <- sub("^f", "Freq.", columns)
+columns <- sub("(Body)+", "Body", columns)
+columns <- sub("Acc", ".Accelaration", columns)
+columns <- sub("Mag", ".Magnitude", columns)
+columns <- sub("Gyro", ".AngularVelocity", columns)
+columns <- sub("mean", "Mean", columns)
+columns <- sub("std", "StandardDeviation", columns)
+columns <- sub("\\.(\\.)+", "", columns)
+columns <- sub("X$", ".X", columns)
+columns <- sub("Y$", ".Y", columns)
+columns <- sub("Z$", ".Z", columns)
 
-colnames(mergedData) = columns
+colnames(mergedData) <- columns
 
 #
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 #
 
 library(plyr)
-tidyData = aggregate(. ~ Subject + Activity, mergedData, mean)
+tidyData <- aggregate(. ~ Subject + Activity, mergedData, mean)
 write.table(tidyData, file = "./data/tidyData.txt", row.names = F)
